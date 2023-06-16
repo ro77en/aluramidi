66b8c35b7e216ac72114f310ee55a54e1f96f2da
lucas <lucas.mdantas@icloud.com>
2023-06-16 12:01:59 -0300

    Otimizando o código com texto dinâmico e função anônima

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-// Pom -----------------------------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idAudioElement) {
+    document.querySelector(idAudioElement).play(idAudioElement);
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+
+let index = 0;
+while (index < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[index];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    index ++;
+
+    console.log(index);
+}

5d3717ac7d2c08e5b8bd7d8639cb1680680c3f15
lucas <lucas.mdantas@icloud.com>
2023-06-21 02:09:12 -0300

    Corrigindo o código

--- main.js
+++ main.js
@@ -5,8 +5,26 @@ function tocaSom (idAudioElement) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
+/*
 let index = 0;
+while (index < listaDeTeclas.length) {
 
+    const tecla = listaDeTeclas[index];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    index ++;
+
+    console.log(index);
+}
+*/
+
+let index = 0;
 for (let key in listaDeTeclas) {
     const tecla = listaDeTeclas[index];
     const instrumento = tecla.classList[1];
